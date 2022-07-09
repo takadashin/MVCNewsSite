128c400c061218c6d16b134f0fd474f7f386ee6a
ProTaka <ProTaka@DESKTOP-Q9QS6OB>
2022-07-09 09:19:50 -0400

    Add project files.

---
+++ MVCNewsSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCNewsSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
